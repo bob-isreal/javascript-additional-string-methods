75fe75e187e01f4bcaf6f0f5446ead97f4dd723d
Techkny <bobadeprojects@gmail.com>
2019-05-09 12:27:21 +0100

    Added Methods for alternating case, number words, is digit and invert case

--- main/app.js
+++ main/app.js
@@ -35,8 +35,6 @@ String.prototype.wordCount = function() {
 };
 String.prototype.toCurrency = function() {
     let digitPattern = /^[0-9]+[.]?[0-9]{1,2}$/;
-    console.log(digitPattern.test(this));
-
     return digitPattern.test(this)
         ? Number(this)
               .toFixed(2)
@@ -45,7 +43,31 @@ String.prototype.toCurrency = function() {
 };
 String.prototype.fromCurrency = function() {
     let digit = /(\d+,)+/;
-    console.log(digit.test(this));
     return digit.test(this) ? this.replace(/,(?=\d)/g, '') : 'Invalid Entry';
+};
+String.prototype.invertCase = function() {
+    let invertedString = '';
+    for (let index = 0; index < this.length; index++) {
+        /[a-z]/.test(this[index])
+            ? (invertedString += this[index].toUpper())
+            : (invertedString += this[index].toLower());
+    }
+    return invertedString;
+};
+String.prototype.alternatingCase = function() {
+    let alternatedCase = this[0].toLower() + this[1].toUpper();
+    for (let index = 2; index < this.length; index++) {
+        index % 2 == 0
+            ? (alternatedCase += this[index].toLower())
+            : (alternatedCase += this[index].toUpper());
+    }
+    return alternatedCase;
+};
+String.prototype.numberWords = function() {
+    return (pattern = /\d/g.test(this) ? this.match(/\d/g).length : 0);
+};
+
+String.prototype.isDigit = function() {
+    return /^\d$/.test(this);
 };
 module.exports = String;
